f95aca6ce47871ff9ca32383e3457f45bd00b002
anshit-rangra <anshit22rangra@gmail.com>
2026-03-22 12:14:26 +0530

    setting rate limiter

--- backend/AUTH/src/app.ts
+++ backend/AUTH/src/app.ts
@@ -3,7 +3,23 @@ import authRouter from "./routes/auth.route.ts";
 import cookieParser from "cookie-parser"
 import passport from "passport"
 import googleStrategy from "./config/google.config.ts";
+import rateLimit from "express-rate-limit";
+import helmet from 'helmet'
+
+
+
 const app = express();
+
+app.use(helmet())
+
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 30,
+    message: "Too many request from this ip, please try after a minute"
+})
+
+app.use(limiter)
 
 app.use(express.json())
 app.use(cookieParser())
